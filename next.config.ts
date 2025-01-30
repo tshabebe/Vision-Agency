61f296cdc272495c98edcea8822fca6e4ed29c1d
Teshome Abebe <161710654+tshabebe@users.noreply.github.com>
2025-01-30 21:48:18 +0000

    feat: avatar

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ['next-mdx-remote', 'lucide-react'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 
   // We run ESLint and TypeScript separately in the CI pipeline
   eslint: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,24 @@ import FrameSelection from './frameSection';
 import ConfirmOrder from './confirmOrder';
 import ChooseArt from './chooseArt';
 import { checkLoggedIn } from '@/auth/auth';
+import { serverClient } from '@/lib/trpc/serverClient';
+import Image from 'next/image';
+import { Suspense } from 'react';
 
 export default async function CustomizePrint() {
   const isLoggedIn = await checkLoggedIn();
   return (
     <div className="container flex min-h-screen flex-col gap-4">
-      <h1 className="mx-auto font-serif text-4xl uppercase md:text-5xl">
-        Make your custom
-      </h1>
+      <div className="flex items-center justify-center">
+        <h1 className="mx-auto font-serif text-4xl uppercase md:text-5xl">
+          Make your custom
+        </h1>
+        {isLoggedIn && (
+          <Suspense fallback={<div>TS</div>}>
+            <Avatar />
+          </Suspense>
+        )}
+      </div>
       <div className="flex grow flex-col gap-8 lg:flex-row">
         <ChooseArt />
         <div className="flex flex-col justify-center gap-8 px-4">
@@ -19,6 +29,25 @@ export default async function CustomizePrint() {
           <ConfirmOrder isLoggedIn={isLoggedIn} />
         </div>
       </div>
+    </div>
+  );
+}
+
+async function Avatar() {
+  const { avatarUrl, name } = await serverClient.userRouter.getUser();
+  if (!avatarUrl || !name) {
+    return <div>TS</div>;
+  }
+  return (
+    <div className="flex items-center gap-2">
+      <p>{name}</p>
+      <Image
+        src={avatarUrl}
+        alt={name}
+        width={28}
+        height={28}
+        className="ml-auto rounded-full"
+      ></Image>
     </div>
   );
 }
